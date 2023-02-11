70726c2be935666a1d7b3e5597d346cba9820d7a
Igor Kulyaev <kulyaev.igor@gmail.com>
2023-02-11 17:54:59 +0200

    Add eslint, fix all files

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,12 +1,12 @@
-import webpack from "webpack";
-import MiniCssExtractPlugin from "mini-css-extract-plugin";
-import {BuildOptions} from "./types/config";
-export function buildLoaders({isDev}: BuildOptions): webpack.RuleSetRule[] {
+import webpack from 'webpack';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import { BuildOptions } from './types/config';
 
+export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
   const svgLoader = {
     test: /\.svg$/,
     use: ['@svgr/webpack'],
-  }
+  };
 
   const babelLoader = {
     test: /\.(js|jsx|tsx)$/,
@@ -15,18 +15,18 @@ export function buildLoaders({isDev}: BuildOptions): webpack.RuleSetRule[] {
       loader: 'babel-loader',
       options: {
         presets: ['@babel/preset-env'],
-        "plugins": [
+        plugins: [
           [
-            "i18next-extract",
+            'i18next-extract',
             {
               locales: ['ru', 'en'],
-              keyAsDefaultValue: true
-            }
+              keyAsDefaultValue: true,
+            },
           ],
-        ]
-      }
-    }
-  }
+        ],
+      },
+    },
+  };
 
   const cssLoader = {
     test: /\.s[ac]ss$/i,
@@ -35,7 +35,7 @@ export function buildLoaders({isDev}: BuildOptions): webpack.RuleSetRule[] {
       isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
       // Translates CSS into CommonJS
       {
-        loader: "css-loader",
+        loader: 'css-loader',
         options: {
           modules: {
             auto: (resPath: string) => resPath.includes('.module.'),
@@ -43,28 +43,28 @@ export function buildLoaders({isDev}: BuildOptions): webpack.RuleSetRule[] {
               ? '[path][name]__[local]--[hash:base64:5]'
               : '[hash:base64:8]',
           },
-        }
+        },
       },
       // Compiles Sass to CSS
-      "sass-loader",
+      'sass-loader',
     ],
-  }
+  };
 
   // Если не используем тайпскрипт - нужен babel-loader
   const typescriptLoader = {
     test: /\.tsx?$/,
     use: 'ts-loader',
     exclude: /node_modules/,
-  }
+  };
 
   const fileLoader = {
-      test: /\.(png|jpe?g|gif|woff2|woff)$/i,
-      use: [
-        {
-          loader: 'file-loader',
-        },
-      ],
-    }
+    test: /\.(png|jpe?g|gif|woff2|woff)$/i,
+    use: [
+      {
+        loader: 'file-loader',
+      },
+    ],
+  };
 
   return [
     fileLoader,
@@ -72,5 +72,5 @@ export function buildLoaders({isDev}: BuildOptions): webpack.RuleSetRule[] {
     babelLoader,
     typescriptLoader,
     cssLoader,
-  ]
+  ];
 }
