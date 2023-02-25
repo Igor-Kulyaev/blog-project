98471b70f5be40e3850ec13a8c81f7236051d658
Igor Kulyaev <kulyaev.igor@gmail.com>
2023-02-25 16:38:26 +0200

    add redux store, add first entity

--- src/entities/Counter/ui/Counter.tsx
+++ src/entities/Counter/ui/Counter.tsx
@@ -1,12 +1,14 @@
 import { Button } from 'shared/ui/Button/Button';
 import { useDispatch, useSelector } from 'react-redux';
 import { StateSchema } from 'app/providers/StoreProvider/config/StateSchema';
+import { useTranslation } from 'react-i18next';
 import { getCounterValue } from '../model/selectors/getCounterValue/getCounterValue';
 import { counterActions } from '../model/slice/counterSlice';
 
 export const Counter = () => {
   const dispatch = useDispatch();
   const counterValue = useSelector(getCounterValue);
+  const { t } = useTranslation();
   const increment = () => {
     dispatch(counterActions.increment());
   };
@@ -17,18 +19,18 @@ export const Counter = () => {
 
   return (
     <div>
-      <h1>
-        {counterValue}
-      </h1>
+      <h1 data-testid="value-title">{counterValue}</h1>
       <Button
         onClick={increment}
+        data-testid="increment-btn"
       >
-        +
+        {t('increment')}
       </Button>
       <Button
+        data-testid="decrement-btn"
         onClick={decrement}
       >
-        -
+        {t('decrement')}
       </Button>
     </div>
   );
